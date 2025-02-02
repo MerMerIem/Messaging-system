18ac80d5da92e83be92656c1db3bbe7576d64677
kellour77 <kellour77@gmail.com>
2025-02-02 01:00:41 +0000

    feat: add server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,30 @@
-import express from 'express';
+import express, { json, urlencoded } from 'express';
+import cors from 'cors';
+// import { config } from 'dotenv';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import cors from 'cors';
+import router from './routes/authenticationRoute.js';
+// import mysql from 'mysql2/promise';
+
+// config();
 
 const app = express();
+ 
 app.use(cors());
+app.use(json());
+app.use('/',router);
 
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
-    credentials: true
-  }
+  },
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log('Waiting for connections...');
+  console.log(`Server is running on port ${PORT}`);
 });
